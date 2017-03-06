10ea15047e8596edfeb1a49a31d65a86c8e23381
Joakim Hovlandsvåg <joakim.hovlandsvag@gmail.com>
2017-03-06 20:12:09 +0100

    Fix setup for testing

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 #!/bin/env python
+# -*- encoding: utf8 -*-
 from setuptools import setup
 
 setup(name='todoist_gtd_cleaner',
       version='0.1',
       desction="Utility fixes for when forcing GTD in Todoist",
-      #url="TODO",
+      url="example.com",
       author="Joakim S. Hovlandsvåg",
       author_email="joakim.hovlandsvag@gmail.com",
       license="GPLv3",
---
+++ todoist_gtd_cleaner/__init__.py
@@ -0,0 +1,4 @@
+#!/bin/env python
+
+def test():
+    return "Well hello"

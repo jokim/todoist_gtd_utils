9fd5d20208c433edb675d7c998ce78ccf31c5eaf
Joakim Hovlandsvåg <joakim.hovlandsvag@gmail.com>
2017-03-06 19:33:13 +0100

    Add more boilerplate to the package
    Trying to create a python package as it's supposed to be set up.

---
+++ README.rst
@@ -0,0 +1,4 @@
+Todoist GTD cleaner
+===================
+
+Simple utility script(s) for fixing missing GTD functionality in Todoist.
---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/bin/env python
+from setuptools import setup
+
+setup(name='todoist_gtd_cleaner',
+      version='0.1',
+      desction="Utility fixes for when forcing GTD in Todoist",
+      #url="TODO",
+      author="Joakim S. Hovlandsvåg",
+      author_email="joakim.hovlandsvag@gmail.com",
+      license="GPLv3",
+      packages=['todoist_gtd_cleaner'],
+      #zip_safe=False,
+      )
+
